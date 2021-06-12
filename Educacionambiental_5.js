b83adf9bc4b09a39d39036924e390db4a99f925d
ecoideas <85766215+ecoideas@users.noreply.github.com>
2021-06-12 15:26:12 -0500

    Add files via upload

---
+++ Educacionambiental_5.js
@@ -0,0 +1,22 @@
+var json_Educacionambiental_5 = {
+"type": "FeatureCollection",
+"name": "Educacionambiental_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "San Paulo", "CURSOS": "3", "TEMAS": "Ambiente", "2019": "Si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9160722207, 4.9635579584 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Sol naciente", "CURSOS": "3", "TEMAS": "Ambiente", "2019": "Si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9138940964, 4.963791875 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "San Luis Gonzaga", "CURSOS": "primaria", "TEMAS": "teatro", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9126505506, 4.9660674724 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Alto Manantial", "CURSOS": "primaria", "TEMAS": "teatro", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9325514842, 4.9317790981 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "La Diana", "CURSOS": "Primaria", "TEMAS": "Ambiente", "2019": "Si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9556058154, 4.9452333427 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Tecnico Industrial", "CURSOS": "Primaria", "TEMAS": "Ambiente", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9270997267, 4.9621138166 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Verganzo", "CURSOS": "Primaria", "TEMAS": "Ambiente", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9367596318, 4.9566263144 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "La Esmeralda", "CURSOS": "Primaria", "TEMAS": "Ambiente", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.8996726823, 4.9685029811 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Divino niño", "CURSOS": "general", "TEMAS": "Afiches", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9138048775, 4.9672057131 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "La Fuente", "CURSOS": "general", "TEMAS": "Afiches", "2019": "si", "TIPO": "I Ed", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9390361135, 5.0039348117 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "MAXO", "CURSOS": null, "TEMAS": "Cuenca", "2019": null, "TIPO": "Industr", "COBERTURA": null }, "geometry": { "type": "Point", "coordinates": [ -73.9352118496, 4.9501213236 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Singha farms", "CURSOS": null, "TEMAS": "Agua", "2019": null, "TIPO": "Floric", "COBERTURA": 118.0 }, "geometry": { "type": "Point", "coordinates": [ -73.9209675734, 4.9804368958 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Alexandra farms", "CURSOS": null, "TEMAS": "Agua", "2019": null, "TIPO": "Floric", "COBERTURA": 56.0 }, "geometry": { "type": "Point", "coordinates": [ -73.9123429493, 4.9933404446 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "Alexandra farms", "CURSOS": null, "TEMAS": "Energia", "2019": null, "TIPO": "Floric", "COBERTURA": 53.0 }, "geometry": { "type": "Point", "coordinates": [ -73.9123429493, 4.9933404446 ] } },
+{ "type": "Feature", "properties": { "id": null, "NOMBRE": "ESCUELA CANAVITA", "CURSOS": "PRIMARIA", "TEMAS": "TEATRO AMBIENTAL", "2019": "Si", "TIPO": "I Ed", "COBERTURA": 200.0 }, "geometry": { "type": "Point", "coordinates": [ -73.9252774044, 4.9484790627 ] } }
+]
+}
